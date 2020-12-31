78165e9848ff537fba1a4315e9ee666364c915a3
Haydn Morris <haydn@energiebespaarders.nl>
2020-12-31 11:34:09 +0100

    add build

---
+++ build/precache-manifest.746b2e969ea81b0cdbbe42aa2fdfde9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873e459d22598cdfd22b5adae1881b76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993bf7aa8abaad29cd43",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ed23eb91d2309b61d40f",
+    "url": "/static/css/main.0a6b0295.chunk.css"
+  },
+  {
+    "revision": "993bf7aa8abaad29cd43",
+    "url": "/static/js/2.5dddef61.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5dddef61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed23eb91d2309b61d40f",
+    "url": "/static/js/main.b406be0b.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
